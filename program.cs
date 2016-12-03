ad400de04225f0277900d368185489794437330c
asjain23456736233545545454543345 <komaltripathi4199@gmail.com>
2016-12-03 15:58:49 +0530

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace sanjay
+{
+    public class overconstruct
+    {
+        public overconstruct()
+        {
+            Console.WriteLine("\n1.this is default constructor\n");
+        }
+        public overconstruct(string name)
+        {
+            Console.WriteLine("2.constructor with out parameter");
+            Console.WriteLine("parameter:\nname{0}\n,name");
+        }
+        public overconstruct(string name, int num)
+        {
+            Console.WriteLine("3.constructor with one parameter");
+            Console.WriteLine("parameter:\nname={0}\nnum ={1}n", name, num);
+        }
+        public void asg()
+        {
+            Console.WriteLine("this is the example of constructor overloading");
+        }
+    }
+    public class demo
+    {
+        public static void main()
+        {
+            Console.WriteLine("enter parameter");
+            string str = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            overconstruct oc1 = new overconstruct();
+            overconstruct oc2 = new overconstruct(str);
+            overconstruct oc3 = new overconstruct(str, num);
+            oc1.asg();
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+    
+    
+       
+        
+        
+    
+
